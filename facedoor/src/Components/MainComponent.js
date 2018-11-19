9f1f9d9d9c5e82d4d22b8e83c6eb1c7e8fae0a74
Kush Jain <kjain14@jainfamily.com>
2018-11-19 16:01:46 -0600

    Pushed buggy code

--- facedoor/src/Components/MainComponent.js
+++ facedoor/src/Components/MainComponent.js
@@ -8,71 +8,95 @@ import alan_image from './DatabaseImage.js'
 import credentials from '../creds';
 require('dotenv').config() //process.env.SECRET_KEY
 //import ImgToBase64 from 'react-native-image-base64';
- 
+
 function getBinary(base64Image) {
-        var binaryImg = atob(base64Image);
-        var length = binaryImg.length;
-        var ab = new ArrayBuffer(length);
-        var ua = new Uint8Array(ab);
-        for (var i = 0; i < length; i++) {
-          ua[i] = binaryImg.charCodeAt(i);
-        }
+  var binaryImg = atob(base64Image);
+  var length = binaryImg.length;
+  var ab = new ArrayBuffer(length);
+  var ua = new Uint8Array(ab);
+  for (var i = 0; i < length; i++) {
+    ua[i] = binaryImg.charCodeAt(i);
+  }
 
-        var blob = new Blob([ab], {
-          type: "image/jpeg"
-        });
+  var blob = new Blob([ab], {
+    type: "image/jpeg"
+  });
 
-        return ab;
-      }
+  return ab;
+}
 
 class App extends Component {
-    onTakePhoto (dataUri) {
-        // Make rest call
-        // Convert test image to base64... should really be done once on startup(look at react-html5-camera-photo' api)
-        var targetBytes = alan_image;
-        AWS.config.update(credentials);
-        dataUri = dataUri.split("data:image/png;base64,")[1];
-
-       // ImgToBase64.getBase64String('../Images/alan.jpg', (err, base64string) => setValue(base64string));
-        if (targetBytes === null) {
-          console.log("FAIL");
-          return;
-        }
-        var params = {
-          SourceImage: {
-           Bytes: getBinary(dataUri)
-          }, 
-          TargetImage: {
-           Bytes: getBinary(targetBytes)
-          },
-          SimilarityThreshold: 90,
-         };
-        var rekognition = new AWS.Rekognition();
-        rekognition.compareFaces(params, function (err, data) {
-          if (err) console.log(err, err.stack); // an error occurred
-          else  {
-            console.log(data)
-            if (data.FaceMatches.length > 0) {
-              alert("ACCESS GRANTED!")
-            } else {
-              alert ("ACCESS DENIED!")
-            }
-          }        // successful response
-        });
-      
-        
-      }
-
-    render() {
-      return (
-        <div className="App">
-        <CardHeader id='cardHeader' title='Take a photo!'/>
-        <Camera
-          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
-        />
-        </div>
-      );
+  compare = function(targetBytes, dataUri){
+    AWS.config.update(credentials);
+    dataUri = dataUri.split("data:image/png;base64,")[1];
+    // ImgToBase64.getBase64String('../Images/alan.jpg', (err, base64string) => setValue(base64string));
+    if (targetBytes === null) {
+      console.log("FAIL");
+      return;
     }
+    var params = {
+      SourceImage: {
+        Bytes: getBinary(dataUri)
+      },
+      TargetImage: {
+        Bytes: getBinary(targetBytes)
+      },
+      SimilarityThreshold: 90,
+    };
+    var rekognition = new AWS.Rekognition();
+    rekognition.compareFaces(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else  {
+        console.log(data)
+        if (data.FaceMatches.length > 0) {
+          alert("ACCESS GRANTED!")
+        } else {
+          alert ("ACCESS DENIED!")
+        }
+      }        // successful response
+    });
   }
-  
-  export default App;
+
+  readImage = function(url, dataUri, callback) {
+    var request = new
+    XMLHttpRequest();   request.onload = function() {
+      var file = new FileReader();
+      file.onloadend = function() {
+        this.compare(file.result, dataUri);
+      }
+      file.readAsDataURL(request.response);
+    };
+    request.open('GET', url);
+    request.responseType = 'blob';
+    request.send();
+  }
+
+  onTakePhoto (dataUri) {
+    // Make rest call
+    // Convert test image to base64... should really be done once on startup(look at react-html5-camera-photo' api)
+   this.readImage("./images/alan.jpg", dataUri);
+
+
+}
+callback(){
+
+}
+// callback(base64, dataUri) {
+//     console.log(base64);
+//     var tgt = base64.split(',')[1];
+//     this.compare(dataUri, tgt);
+//  }
+
+render() {
+  return (
+    <div className="App">
+    <CardHeader id='cardHeader' title='Take a photo!'/>
+    <Camera
+    onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+    />
+    </div>
+  );
+}
+}
+
+export default App;
